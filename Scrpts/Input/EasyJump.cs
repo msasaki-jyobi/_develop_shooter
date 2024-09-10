f0b1d850400de2d8f382b5ceca95bf6613bd1ee3
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-09-10 11:30:14 +0900

    Update Jump + Move

---
+++ Scrpts/Input/EasyJump.cs
@@ -0,0 +1,33 @@
+using GameSet.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyJump : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    [SerializeField] private float _jumpPower = 30f;
+    [SerializeField] private LineData _groundData;
+
+    [SerializeField] private float _drag = 0;
+    [SerializeField] private float _angularDrag = 0.05f;
+
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.drag = _drag;
+        _rigidBody.angularDrag = _angularDrag;
+        _rigidBody.interpolation = RigidbodyInterpolation.None;
+    }
+
+    private void Update()
+    {
+        bool check = UtilityFunction.CheckLineData(_groundData, transform);
+        if (check)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                _rigidBody.AddForce(transform.up * _jumpPower, ForceMode.Impulse);
+        }
+    }
+}
